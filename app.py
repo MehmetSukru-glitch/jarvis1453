8a6a68bb880a0c5b199ff9624ac1e2f4edbcaa18
MehmetSukru <sukrusevinc181@gmail.com>
2026-05-12 21:23:10 +0300

    host guncellendi

--- app.py
+++ app.py
@@ -197,4 +197,5 @@ def komut():
     return jsonify({"cevap": cevap})
 
 if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
     app.run(debug=True)
